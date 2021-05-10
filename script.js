c00a60c0ff51fda62bbd61194fa97c8b84e4e759
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-05-10 17:47:10 +0100

    media fixes

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ const boxes = document.querySelectorAll(".box");
 checkboxEl.addEventListener("change", function () {
   document.body.classList.toggle("dark");
   topBar.classList.toggle("darktopBar");
+  darkBoxes();
+});
 
+function darkBoxes() {
   boxes.forEach((box) => box.classList.toggle("darkBox"));
   topboxes.forEach((box) => box.classList.toggle("darkBox"));
   botboxes.forEach((box) => box.classList.toggle("darkBox"));
-});
+
+  boxes.forEach((box) => box.classList.toggle("darkHover"));
+  topboxes.forEach((box) => box.classList.toggle("darkHover"));
+  botboxes.forEach((box) => box.classList.toggle("darkHover"));
+}
